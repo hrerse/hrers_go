235b0ae632c386836603122e62611b8b35b43d7b
hrerse <hrerse@gmail.com>
2018-07-31 13:59:54 +0800

    列表练习

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ number=20
 guess=-1
 print(type(guess))
 #print("数字猜谜游戏")
-lis = [10, 2.0, "my name:", [1, 2]]
-lis.append(10)
+lis = []
+#lis.append(10)
 while guess != number :
     guess = input("Please input the number:\n")
     print(type(guess))
     if guess.isdigit() :
         guess = int(guess)
+        lis.append(guess)
         if guess==number :
             print("恭喜您，猜中了")
 #            input("Please Enter a key to exit")
@@ -19,7 +20,7 @@ while guess != number :
             print("猜小了")
     else :
         print("Please input a number")
-
-i = 0
+# not len(lis[])
+print("You guessed %d times totally" % len(lis))
 for x in lis :
     print(x)
